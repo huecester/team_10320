c7955231e00de1f70f82e662603b4771081a29e5
huecester <jericho.paulino@yahoo.com.ph>
2022-01-21 14:59:39 -0800

    Add sliding

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualTeleOp.java
@@ -31,16 +31,41 @@ public class ManualTeleOp extends LinearOpMode {
 		waitForStart();
 
 		while (opModeIsActive()) {
-			// driving
+			// parameters
+			final double threshold = 0.05;
+
+			// `turning/forward
 			double drive = -gamepad1.left_stick_y;
 			double turn = gamepad1.right_stick_x;
 			double leftPower = Range.clip(drive + turn, -1.0, 1.0);
 			double rightPower = Range.clip(drive - turn, -1.0, 1.0);
 
-			frontLeft.setPower(leftPower);
-			backLeft.setPower(leftPower);
-			frontRight.setPower(rightPower);
-			backRight.setPower(rightPower);
+			// sliding
+			boolean leftSlide = gamepad1.dpad_left;
+			boolean rightSlide = gamepad1.dpad_right;
+
+			if (Math.abs(drive) >= threshold || Math.abs(turn) >= threshold) {
+				frontLeft.setPower(leftPower);
+				backLeft.setPower(leftPower);
+				frontRight.setPower(rightPower);
+				backRight.setPower(rightPower);
+			} else if (leftSlide) {
+				frontLeft.setPower(1);
+				backLeft.setPower(-1);
+				frontRight.setPower(-1);
+				backRight.setPower(1);
+			} else if (rightSlide) {
+				frontLeft.setPower(-1);
+				backLeft.setPower(1);
+				frontRight.setPower(1);
+				backRight.setPower(-1);
+			} else {
+				frontLeft.setPower(0);
+				backLeft.setPower(0);
+				frontRight.setPower(0);
+				backRight.setPower(0);
+			}
+
 
 			telemetry.addData("Wheels", "left (%.2f), right (%.2f)", leftPower, rightPower);
 			telemetry.update();
