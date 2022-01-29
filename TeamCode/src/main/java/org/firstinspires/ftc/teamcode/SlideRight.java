1da4f6e59306059e7b4f4da03c263951ac7aa127
huecester <jericho.paulino@yahoo.com.ph>
2022-01-28 22:26:11 -0800

    Auton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideRight.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+	import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+	import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+	import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous
+public class SlideRight extends LinearOpMode {
+	@Override
+	public void runOpMode() {
+		final double power = 0.5;
+
+		DcMotor frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+		DcMotor backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+		DcMotor frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+		DcMotor backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+		waitForStart();
+
+		if (opModeIsActive()) {
+			frontLeft.setPower(power);
+			backLeft.setPower(-power);
+			frontRight.setPower(-power);
+			backRight.setPower(power);
+
+			sleep(10000);
+
+			frontLeft.setPower(0);
+			backLeft.setPower(0);
+			frontRight.setPower(0);
+			backRight.setPower(0);
+		}
+	}
+}
